21cff38aa23ac188d0b7ad7d42eb442113b031ba
Doug Campbell <dcampbell@gogoair.com>
2017-05-12 15:29:12 -0500

    playing around with slack rtm for processing messages

--- main.go
+++ main.go
@@ -8,13 +8,22 @@ import (
 
 func main() {
 	api := slack.New(os.Getenv("SLACK_TOKEN"))
-	api.SetDebug(true)
+	//api.SetDebug(true)
 	log.Println("Slack Bot Starting")
 
 	rtm := api.NewRTM()
 	go rtm.ManageConnection()
+	info := rtm.GetInfo()
+	log.Printf("Bot User: %v", info.User.Name)
+	botID := info.User.ID
 
 	for msg := range rtm.IncomingEvents {
-		log.Println("Event Recieved: ", msg)
+		log.Println("Event Recieved: ")
+		switch ev := msg.Data.(type) {
+		case *slack.MessageEvent:
+			log.Printf("Message: %v %v", ev.Text, ev.User)
+		default:
+			// ignore other events
+		}
 	}
 }
