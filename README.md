853769a1135a9e114c9650b0834f494a2617f623
Doug Campbell <dcampbell@gogoair.com>
2017-05-12 20:58:01 -0500

    watching direct messages and able to send base response

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ Future Features:
 - Open to any suggestions
 
 This is very much a work in progress and just a fun side-project for now.
+
+## Scheduling Idea
+
+My intial idea is to use https://godoc.org/github.com/robfig/cron for scheduling the stand ups
+The user messages the bot with "add $cron_expression $user1 $user2 $user3" 
+This will add a standup that messages all users based on the cron_expression.
+
+
--- main.go
+++ main.go
@@ -1,29 +1,57 @@
 package main
 
 import (
+	"fmt"
 	"github.com/nlopes/slack"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
+	token := os.Getenv("SLACK_TOKEN")
+	if token == "" {
+		log.Println("SLACK_TOKEN not found")
+		os.Exit(1)
+	}
 	api := slack.New(os.Getenv("SLACK_TOKEN"))
 	//api.SetDebug(true)
 	log.Println("Slack Bot Starting")
 
 	rtm := api.NewRTM()
 	go rtm.ManageConnection()
-	info := rtm.GetInfo()
-	log.Printf("Bot User: %v", info.User.Name)
-	botID := info.User.ID
 
 	for msg := range rtm.IncomingEvents {
-		log.Println("Event Recieved: ")
 		switch ev := msg.Data.(type) {
 		case *slack.MessageEvent:
-			log.Printf("Message: %v %v", ev.Text, ev.User)
+			info := rtm.GetInfo()
+			if ev.User != info.User.ID { //verifies the message is not from the bot
+				if info.GetIMByID(ev.Channel).IsIM {
+					log.Printf("Direct message: %s", ev.Text)
+					go respond(rtm, ev)
+				} else {
+					log.Printf("Message: %v %v", ev.Text, ev.User)
+				}
+			}
+		case *slack.RTMError:
+			log.Printf("Error: %s\n", ev.Error())
+		case *slack.InvalidAuthEvent:
+			log.Printf("Invalid credentials")
+			os.Exit(1)
 		default:
 			// ignore other events
 		}
 	}
+}
+
+//function to handle direct messages to bot
+func respond(rtm *slack.RTM, msg *slack.MessageEvent) {
+	var response string
+	args := strings.Split(msg.Text, " ")
+	switch args[0] {
+	case "help":
+		help := "Supported commands:\n"
+		response = fmt.Sprintf(help)
+	}
+	rtm.SendMessage(rtm.NewOutgoingMessage(response, msg.Channel))
 }
